a288bece3d4f571a1eb436ee6079e08f7470a8ad
mkalksztejn <mat.kalksztejn@wp.pl>
2022-11-14 10:13:01 +0100

    init

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyAEwqh4bjitmNGxr2fz3okjNQMFb1-mnXc",
+    authDomain: "tomatochat-897bd.firebaseapp.com",
+    projectId: "tomatochat-897bd",
+    storageBucket: "tomatochat-897bd.appspot.com",
+    messagingSenderId: "424794730941",
+    appId: "1:424794730941:web:94ea516de22ffafb75e2d4",
+    measurementId: "G-7QT98GGCQE"
+  },
+  production: true
+};
